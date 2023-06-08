876b16ae2e9c722b800e9723fba20d75513d6759
Edgar Melo <edgarmelo@acad.ifma.edu.br>
2023-06-08 18:06:24 -0300

    interação entre objetos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,183 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Armazenador from './componentes/Armazenador';
+
+class App extends Component {
+  state = {
+    armazenadorStateArray:[
+      {
+        id: 1,
+        titulo: 'Meu Projeto Tcc',
+        area: 'Computação / Ciencia de Dados',
+        data: new Date(2023, 3, 22, 12, 15, 0),
+        resumo: 'Resumo do projeto 01'
+      },
+      {
+        id: 2,
+        titulo: 'Meu Projeto Dissertacao',
+        area: 'Computação / Ciencia de Dados',
+        data: new Date(2023, 3, 22, 12, 15, 0),
+        resumo: 'Resumo do projeto 02'
+      }
+    ],
+    armazenadorStateREQUEST: {
+      titulo: '',
+      area: '',
+      data: '',
+      resumo: ''
+    }
+  }
+
+  adicionarTCCMocks = () => {
+    console.log("Adicionando NOVO TCC...");
+    const novoTccMock = {
+        id: 3,
+        titulo: 'Meu Projeto Mock Incluido VIA ESTADO',
+        area: 'Computação Mock State/ Mock State',
+        data: new Date(2023, 3, 22, 12, 15, 0),
+        resumo: 'Resumo do Texto mockado adicionado'
+    }
+
+    /**obtem a lista armazenada no array de estados existentes */
+    /**MOD1 - criando lista e adicionando valor */
+    /*
+    let lista = this.state.armazenadorStateArray;
+    lista.push(novoTccMock); // add o novo objeto dentro da lista 
+    this.setState({armazenadorStateArray: lista}); //inclui na lista de estados a nova lista
+    */
+
+    /**MOD 2 -utilizando spred para incluir na lista de estados */
+    this.setState({armazenadorStateArray: [...this.state.armazenadorStateArray, novoTccMock]})
+
+  }
+
+  adicionarTCCInput = (evento) => {
+    evento.preventDefault(); /* anula o envnto POST informado lá no form */
+
+    const stateREQUEST = this.state.armazenadorStateREQUEST;
+    stateREQUEST.data = new Date();
+    this.setState({armazenadorStateArray: [...this.state.armazenadorStateArray, stateREQUEST], //captura o que ja existe no array de estado e inclui o novo estado
+    armazenadorStateREQUEST:{ //lmpando o campo input após setar o valores no novo estado
+      titulo: '',
+      area: '',
+      data: '',
+      resumo: ''
+    }}) 
+  }
+
+  /*responsael por preencher o estado do objeto com a digitação */
+  mudarEstadoAoDigitarTCC = evento => {
+    /*const value = evento.target.value; /** obtem o valor do evento */
+    /*const campoPosicao = evento.target.name; /** obtem o nome do evento */
+    
+    /*this.setState({armazenadorStateREQUEST: {...this.armazenadorStateREQUEST, titulo: value} })*/ //dessa forma deve ser criado um pra cada
+
+    const {name, value} = evento.target;
+    this.setState({armazenadorStateREQUEST: {...this.state.armazenadorStateREQUEST, [name]: value} }) //dessa forma captura buscando pelo name e value do evento (ou seja, name e value do campo input)
+  }
+
+  removerTcc = titulo => {
+    let list = this.state.armazenadorStateArray;
+
+    list = list.filter(t => t !== titulo)
+    this.setState({armazenadorStateArray: list})
+  }
+
+  render() {
+    return (
+
+      <div className="App">
+        
+         <div className='styles'>
+            <h1>Renderizando o componente 1- SET HARD CODE</h1>
+            <Armazenador indice="1" numero1="3" numero2="2" inteiro1={1} inteiro2={2} 
+            titulo="Ocupação dos Manguezais Ludovicenses" area="Biologia  / Bioma / Ciencia da natureza"
+            data={new Date(2023, 3, 19)}>
+              Texto dentro do componente 1 (children)
+              <p>OBS: Aqui dentro pode passar um componente</p>
+            </Armazenador>
+
+            <h1>Renderizando o componente 2 - SET HARD CODE</h1>
+            <Armazenador indice="2" numero1="5" numero2="6" inteiro1={7} inteiro2={3}
+            titulo="Facilidades do uso de react" area="Computação / Ciencia de dados"
+            data={new Date(2022, 2, 15)}>
+              Texto dentro do componente 2 (children)
+            </Armazenador>
+
+
+            <h1>Renderizando o componente 3 COM STATE - OBJETOS MOCK</h1>
+
+            {this.state.armazenadorStateArray.map((armazenadoParam, indice) => (
+
+                <Armazenador 
+                key={indice}
+                titulo={armazenadoParam.titulo} 
+                area={armazenadoParam.area}
+                data={armazenadoParam.data}
+                resumo={armazenadoParam.resumo}
+                onRemove={this.removerTcc.bind(this, armazenadoParam)}
+                >
+
+                </Armazenador>
+
+            ))}
+
+
+            <h1>Renderizando o componente 4 COM STATE - MANIPULANDO ESTADO</h1>
+            <button onClick={this.adicionarTCCMocks}> Adicionado um TCC com Evento Mock </button>
+            <br></br>
+
+            <br></br>
+
+            
+            <form method="post" onSubmit={this.adicionarTCCInput} /** onSubmit modifica o Array de Estado de acordo com o que vem do formulário*/>
+              
+              <h3>Adicionando um Novo TCC Via Input</h3>
+              <input
+              type="text"
+              name="titulo"
+              onChange={this.mudarEstadoAoDigitarTCC} /**muda o estado ao digitar */
+              value={this.state.armazenadorStateREQUEST.titulo} /** recebe o estado no indice titulo */
+            
+              required
+              placeholder="Digite o Título" 
+              />
+              <input
+              type="text"
+              name="area"
+              onChange={this.mudarEstadoAoDigitarTCC} /**muda o estado ao digitar */
+              value={this.state.armazenadorStateREQUEST.area}
+              required
+              placeholder="Digite Area" 
+              />
+              <textarea
+                name="resumo"
+                onChange={this.mudarEstadoAoDigitarTCC} /**muda o estado ao digitar */
+                value={this.state.armazenadorStateREQUEST.resumo}
+                required
+                rows="5" 
+              />
+
+            <button 
+            /*onClick={this.adicionarTCCInput}*/
+            type='submit' > 
+            add Via Input 
+            </button>
+
+            </form>
+
+         </div>
+        
+
+
+      </div>
+
+    );
+  }
+
+
 }
 
 export default App;
---
+++ src/componentes/Armazenador.js
@@ -0,0 +1,52 @@
+import React from "react";
+import './Armazenador.css'
+
+//JSX  - HTML dentro do JS
+const Armazenador = (props) => // props indica que será obtido os valores das propriedades enviadas nos objeto  que 'instacia' esta classe
+    // props é um objetos que vem as propriedades do componente
+    { // {} indica que a classe tem um retorno 
+    const num1 = 2;
+    const num2 = 5;
+
+    const numParam1 = props.numero1;
+    const numParam2 = props.numero2;
+
+    const intParam1 = props.inteiro1;
+    const intParam2 = props.inteiro2;
+
+
+
+    return ( // tudo que será retornado prcisa estar entre ()
+    //para realizar operação de (soma, divisao, ect), precisa estar entre {}
+    <div className="Armazenador"> 
+        <h1>COMPONENTE ARMAZENADOR {props.indice}</h1>
+
+        <p> Numero parametro 1 enviado: {numParam1}</p>
+        <p> Numero parametro 2 enviado: {numParam2}</p>
+        <h4>Soma dos dois parametros String: {numParam1 + numParam2}</h4>
+        <h4>Soma dos dois parametros Inteiro: {intParam1 + intParam2}</h4>
+
+
+        <div>A soma de 2+5 é: {2+5}</div>
+        <h3>Numero 1: {props.numero1}</h3> 
+        <h3>Numero 2: {props.numero2}</h3> 
+        <h3>Titulo: {props.titulo}</h3> 
+        <h3>Area 1: {props.area}</h3>
+        <h3>Data: {props.data.toString()}</h3>
+        <p>
+            
+            <h4>RESUMO:</h4> {props.resumo}
+        </p>
+        <h3> Texto do componente: {props.children}</h3>
+
+        
+        <div> [mode 2] A soma de 2+5 é: {num1+num2}</div>
+        <button onClick={props.onRemove}>&times;</button> {/** integracao entre os componentes */}
+        
+    </div>
+    )
+
+};
+
+
+export default Armazenador;
